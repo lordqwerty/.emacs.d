f72e22b33926e01a995c21fa0ff8b42e69dcea15
Jorden Whitefield <jorden.whitefield@googlemail.com>
2018-02-28 11:31:16 +0000

    init commit

--- init.el
+++ init.el
@@ -84,23 +84,24 @@
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
-(custom-set-variables
+;;(custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("c620ce43a0b430dcc1b06850e0a84df4ae5141d698d71e17de85e7494377fd81" "59171e7f5270c0f8c28721bb96ae56d35f38a0d86da35eab4001aebbd99271a8" "e9460a84d876da407d9e6accf9ceba453e2f86f8b86076f37c08ad155de8223c" default)))
- '(package-selected-packages
-   (quote
-    (yaml-mode intero smex use-package latex-pretty-symbols markdown-mode haskell-mode atom-one-dark-theme atom-dark-theme))))
-(custom-set-faces
+;; '(custom-safe-themes
+  ;; (quote
+    ;;("c620ce43a0b430dcc1b06850e0a84df4ae5141d698d71e17de85e7494377fd81" "59171e7f5270c0f8c28721bb96ae56d35f38a0d86da35eab4001aebbd99271a8" "e9460a84d876da407d9e6accf9ceba453e2f86f8b86076f37c08ad155de8223c" default)))
+;; '(package-selected-packages
+  ;; (quote
+;;    (yaml-mode intero smex use-package latex-pretty-symbols markdown-mode haskell-mode atom-one-dark-theme atom-dark-theme))))
+;;(custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+;; )
+
 
 (load-theme 'atom-one-dark)
 
@@ -218,11 +219,6 @@
   :ensure t
   :bind (("M-x" . smex))
   :config (smex-initialize))
-
-;; intero itself
-(use-package intero
-  :ensure t)
-(add-hook 'haskell-mode-hook 'intero-mode)
 
 ;;; Development Packages
 
@@ -363,3 +359,12 @@
          ("\\.md\\'" . markdown-mode)
          ("\\.markdown\\'" . markdown-mode))
   :init (setq markdown-command "multimarkdown"))
+
+;; LaTex
+(use-package latex-mode
+  :defer t
+  :commands (latex-mode latex-pretty-symbols)
+  :mode (("\\.tex\\'" . latex-mode)))
+
+(use-package latex-pretty-symbols
+  :defer t)
